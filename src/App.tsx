f24c743612da2f710773e62a05da04e88badc805
alzaabi555 <157557210+alzaabi555@users.noreply.github.com>
2026-03-22 17:45:51 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import RamadanTheme from './components/RamadanTheme'; // 👈 أضف هذا السطر
 import { 
   QrCode, ArrowLeft, Loader2, 
   LogOut, Trophy, ThumbsUp, ThumbsDown, BookOpen, ChevronLeft, 
