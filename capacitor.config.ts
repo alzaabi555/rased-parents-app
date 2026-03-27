1aed71128dbc8cfa038d693310355f70ed09b6bc
alzaabi555 <157557210+alzaabi555@users.noreply.github.com>
2026-03-27 19:38:13 +0400

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.alzaabi.rased.parents',
+  appId: 'com.rased.parents',
   appName: 'راصد لولي الأمر',
   webDir: 'dist'
 };
