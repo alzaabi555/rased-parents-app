888e9a17d057c862d8909c93844b37910594e753
alzaabi555 <157557210+alzaabi555@users.noreply.github.com>
2026-03-27 17:14:02 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { LocalNotifications } from '@capacitor/local-notifications';
 import { 
   ArrowLeft, Loader2, LogOut, Trophy, ThumbsUp, BookOpen, ChevronLeft, 
   MessageSquare, Send, X, Code, User, RefreshCw, HeartHandshake,
   Star, School, Fingerprint, LayoutGrid, Bell, AlertTriangle, 
   ClipboardList, Calendar, History
 } from 'lucide-react';
+import { LocalNotifications } from '@capacitor/local-notifications';
 
 const GOOGLE_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzKPPsQsM_dIttcYSxRLs6LQuvXhT6Qia5TwJ1Tw4ObQ-eZFZeJhV6epXXjxA9_SwWk/exec";
 
