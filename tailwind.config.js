9f148fbc6334589505cd3e0dd7b628933f17cff9
alzaabi555 <157557210+alzaabi555@users.noreply.github.com>
2026-03-27 16:15:36 +0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,14 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // 🚨 هذا السطر هو الأهم! يخبر التصميم أن يقرأ ملف App.tsx
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Cairo', 'sans-serif'], // لضمان ظهور الخط العربي بشكل فخم
+      }
+    },
   },
   plugins: [],
 }
